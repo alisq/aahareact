3adc6e323d1ee791c190eb155ba93216400cace4
donald-zhu <donaldzhu_1010@icloud.com>
2023-02-21 21:39:40 -0500

    updated click

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 
 function App() {
 
+  const handleClick = (msg) => { alert(msg) }
 
 
   return (
@@ -18,26 +19,26 @@ function App() {
           C<span className='red'>\</span>A<span className='red'>\</span>N<span className='red'>\</span>A<span
             className='red'>\</span>D<span className='red'>\</span>A<br />
           WE DEMAND...
-          <h2 data-demand="land-back"><span className="num">¹</span>Land Back!</h2>
-          <h2 data-demand="collective-ownership"><span className="num">²</span>Collective<br />
+          <h2 onClick={() => handleClick('land-back')}><span className="num">¹</span>Land Back!</h2>
+          <h2 onClick={() => handleClick('collective-ownership')}><span className="num">²</span>Collective<br />
             Ownership!</h2>
-          <h2 data-demand="on-the-land-housing"><span className="num">³</span>On the Land Housing<span
+          <h2 onClick={() => handleClick('on-the-land-housing')}><span className="num">³</span>On the Land Housing<span
             className="exAlt2"></span></h2>
-          <h2 data-demand="home-building-lodges"><span className="num">⁴</span>Home <br />
+          <h2 onClick={() => handleClick('home-building-lodges')}><span className="num">⁴</span>Home <br />
             Building Lodges<span className="exAlt2"></span> </h2>
-          <h2 data-demand="reparative-planning"><span className="num">⁵</span>Reparative Planning<span
+          <h2 onClick={() => handleClick('reparative-planning')}><span className="num">⁵</span>Reparative Planning<span
             className="exAlt1"></span> </h2><br />
 
-          <h2 data-demand="intentional-communities-for-unhoused"><span className="num">⁶</span>Intentional Communities
+          <h2 onClick={() => handleClick('intentional-communities-for-unhoused')}><span className="num">⁶</span>Intentional Communities
             for Unhoused <br />
             People! </h2>
-          <h2 data-demand="the-unearned-increment"><span className="num">⁷</span>The Unearned Increment<span
+          <h2 onClick={() => handleClick('the-unearned-increment')}><span className="num">⁷</span>The Unearned Increment<span
             className="exAlt2"></span> </h2>
-          <h2 data-demand="mutual-aid-housing"><span className="num">⁸</span>Mutual <br />
+          <h2 onClick={() => handleClick('mutual-aid-housing')}><span className="num">⁸</span>Mutual <br />
             Aid Housing! </h2>
-          <h2 data-demand="ambient-ecosystems"><span className="num">⁹</span>Ambient Ecosystems <br />
+          <h2 onClick={() => handleClick('ambient-ecosystems')}><span className="num">⁹</span>Ambient Ecosystems <br />
             Commons!</h2>
-          <h2 data-demand="surplus-properties-for-housing"><span className="num">¹⁰ </span>Surplus Properties for
+          <h2 onClick={() => handleClick('surplus-properties-for-housing')}><span className="num">¹⁰ </span>Surplus Properties for
             Housing<span className="exAlt3"></span></h2>
         </div>
       </section>
