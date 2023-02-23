fe2a63336f815c74f28fc4d5e4f205babaa8183d
donald-zhu <donaldzhu_1010@icloud.com>
2023-02-22 20:26:14 -0500

    changed superscripts

--- src/components/demandHeader.jsx
+++ src/components/demandHeader.jsx
@@ -4,11 +4,6 @@ function DemandHeader({ name, num, domText, exMark }) {
   return (
     <h2 onClick={() => handleClick(name)}><span className="num">{num}</span>{domText}<span className={exMark}></span> </h2>
   )
-
-  // return (
-  //   <h2 onClick={() => handleClick(name)}><span className="num">⁴</span>Home <br />
-  //     Building Lodges<span className="exAlt2"></span> </h2>
-  // )
 }
 
 export default DemandHeader
