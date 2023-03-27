63378f37f025c2493eb5e161d6803489141b4fc2
donald-zhu <65828128+donaldzhu@users.noreply.github.com>
2023-03-27 13:22:47 -0400

    Update urlUtil.js

--- src/utils/urlUtil.js
+++ src/utils/urlUtil.js
@@ -1,5 +1,16 @@
-const urlPartIndex = [null, 'lang', 'category', 'content']
 const delimiter = '/'
 export const getUrlParts = location => {
-  const [, lang, category, content] = location.pathname.split('/')
-} 
+  const [, lang, cateogry, content] = location.pathname.split(delimiter)
+  return { lang, cateogry, content }
+}
+
+export const partsToUrl = ({ lang, cateogry, content }) => `/${lang}/${cateogry}/${content}`
+
+export const setUrlParts = (navigate, type, value) => {
+  const urlParts = getUrlParts()
+  if (type === 'lang') urlParts.lang = value
+  if (type === 'content') urlParts.content = value
+  const url = partsToUrl(urlParts)
+  navigate(url)
+  return url
+}
