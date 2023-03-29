f79784c3cc275b59531e87209ad257a7b9c9ed5d
donald-zhu <donaldzhu_1010@icloud.com>
2023-03-29 17:44:43 -0400

    tried client-side routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/*' element={<App />} />
+))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
