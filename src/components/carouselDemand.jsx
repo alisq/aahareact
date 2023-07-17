6adc1b152715405bcdcc613463cfa0321511c8e5
donald-zhu <donaldzhu_1010@icloud.com>
2023-07-17 18:46:37 -0400

    Added flickity pop up

--- src/components/carouselDemand.jsx
+++ src/components/carouselDemand.jsx
@@ -1,39 +1,57 @@
 
+import { useEffect, useRef, useState } from 'react'
 import Flickity from 'react-flickity-component'
 
 const flickityOptions = {
-    initialIndex: 0,
-    draggable: true,
-    wrapAround:true,
-    pageDots:false
-    // prevNextButtons: false
+  initialIndex: 0,
+  draggable: true,
+  wrapAround: true,
+  pageDots: false
+  // prevNextButtons: false
 }
 
-function CarouselDemand({lang, carouselImages}) {
+function CarouselDemand({ lang, carouselImages }) {
+  const [zoomedImgIndex, setZoomImgIndex] = useState(null)
 
-console.log(carouselImages)
+  const imgRefs = useRef([])
 
-    const getImages = (image, i) => 
-    
-    <div className="slide" key={i}>
-      <div className="demand-slide">
-        <img src={"http://server-aaha.codepanel.in/"+image.uri} key={i} alt={image[lang].alt} />
+  useEffect(() => {
+    document.body.style.overflowY = zoomedImgIndex ? 'hidden' : 'initial'
+  }, [zoomedImgIndex])
+
+  const getImages = (image, i) => {
+    const Img = <img src={"http://server-aaha.codepanel.in/" + image.uri} key={i} alt={image[lang].alt} />
+    imgRefs.current.push(Img)
+    return (
+      <div className="slide" key={i} >
+        <div className="demand-slide">
+          {Img}
+        </div>
+        {image[lang].caption !== "" &&
+          <div className="caption text-center">{image[lang].caption.replace(/(<([^>]+)>)/gi, "")}</div>}
       </div>
-        {image[lang].caption !== "" && <div className="caption text-center">{image[lang].caption.replace(/(<([^>]+)>)/gi, "")}</div>}
-    </div>
+    )
+  }
 
   return (
-    <Flickity
-      className={'carousel'} // default ''
-      elementType={'div'} // default 'div'
-      options={flickityOptions} // takes flickity options {}
-      disableImagesLoaded={false} // default false
-      reloadOnUpdate // default false
-      static={false} // default false
-    >
-   {carouselImages.map(getImages)}
-   
-    </Flickity>
+    <>
+      {typeof zoomedImgIndex === 'number' &&
+        <div className='slide-fullscreen' onClick={() => setZoomImgIndex(null)}>
+          {imgRefs.current[zoomedImgIndex]}
+        </div>}
+      <Flickity
+        flickityRef={ref => ref.on('staticClick', (_, __, ___, i) => setZoomImgIndex(i))}
+        className={'carousel'} // default ''
+        elementType={'div'} // default 'div'
+        options={flickityOptions} // takes flickity options {}
+        disableImagesLoaded={false} // default false
+        reloadOnUpdate={false} // default false
+        static={false} // default false
+      >
+        {carouselImages.map(getImages)}
+
+      </Flickity>
+    </>
   )
 }
 
