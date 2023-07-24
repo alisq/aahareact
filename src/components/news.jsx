b65476aeac3e2ccf26d205e0a2c11ed72b9c78d9
alisq <ali.shamas.qadeer@gmail.com>
2023-07-24 13:56:25 -0700

    fixed gallery

---
+++ src/components/news.jsx
@@ -0,0 +1,15 @@
+
+
+function News({lang}) {
+
+    return(
+        <table>
+            <tr><td><label className="label">press</label></td><td>04.17.23</td><td><h3>AAHA at it again!</h3></td><td>CBC Radio</td></tr>
+            <tr><td><label className="label">press</label></td><td>04.17.23</td><td><h3>AAHA at it again!</h3></td><td>CBC Radio</td></tr>
+            <tr><td><label className="label">news</label></td><td>04.17.23</td><td><h3>AAHA site launched</h3></td><td><label className="label--red">read more</label></td></tr>
+        </table>
+    )
+}
+
+
+export default News
